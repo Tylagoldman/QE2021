71ff15c2ee2ce296dc6eb976e3d22bedc9175a94
Tylagoldman <3837300@myuwc.ac.za>
2021-07-13 16:54:30 +0200

    Homework 5, slight update, 13/07/2021

--- Tyla_Goldman_HW5.R
+++ Tyla_Goldman_HW5.R
@@ -52,6 +52,9 @@ sum(env_pca$CA$eig)
 round(env_pca$CA$eig[1] / sum(env_pca$CA$eig) * 100, 1) # result in %
 #see the porion of the first variation from the total variation 
 
+barplot(as.vector(env_pca$CA$eig)/sum(env_pca$CA$eig)) #used to see the change in eigenvalues over principal components 
+#can see which principal components to use in ordination plots 
+
 #QUESTIONS A
 
 #Principal component values become smaller as the value of information it carries decreases 
@@ -161,7 +164,7 @@ ordisurf(birds_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots 
 #in PC2 foliage height diversity index has the highest negative while tree species diversity has the highest positive seen in summary species scores
 #therefore on the pca ordination display according to scaling 1, sites in the region of higher/ positive PC2 axes are defined by high tree species diversity(such as site 12 and 22) 
 #and sites in the region of lower/negative PC2 axes are defined by low foliage height diversity 
-#Due to variables, elevation and foliage height diversity index vectors being outside the radius of the ordination display 
+#Due to variables, elevation and foliage height diversity index vectors being outside the radius of the circloe of equilibrium contribution of the ordination display 
 #they can strongly direct one towards patterns where ground cover is high when elevation is high 
 #or low secondary tree cover and tree species diversity can relate to low foliage height diversity   
 
@@ -175,7 +178,8 @@ ordisurf(birds_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots 
 #which is not seen at lower elevations. 
 
 #C
-#According to the variables mainly influencing variation, negative correlation is seen among secondary tree cover and ground cover,  
+#According to the variables mainly influencing variation, negative correlation is seen among secondary tree cover and ground cover, 
+#this correlation is graphically seen in scaling two as angle among variable vectors are greater than 90 degrees 
 #ground cover is high when secondary tree cover is low as more sunlight is reaching the ground allowing for more shrub like plants to grow 
 #conversely when there is high secondary tree cover, the higher trees maximize the sunlight, only allowing trees on the ground to grow in regions
 #where remaining sunlight reaches 
@@ -185,6 +189,13 @@ ordisurf(birds_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots 
 #that reproduce over space much faster causing a lower species diversity as theyve colonized a region well
 #and they will alter the conditions for lower growing trees causing some species unfit to grow at lower regions thus the tree species 
 #diversity is low with multiple canopies deemed by a high foliage high diversity index
+
+#exposure and elevation was matched above and their positive correlation is supported by the small angle among variable vectors in scalling two graph 
+
+#To touch on another impactful environmental variable among sites with regatds to its vector reaching out of the radius in scaling one
+#the tree basal are is positively correlated to foliage height diversity index as a small angle exists among vector. This exists mechanistically as 
+#the higher the basal area of tree becomes, the more established the larger tree is thus influencing higher canopies in foresty area
+#resultng in higher foliage height diversity index 
 
 #alpine plant communities environmental data 
 
@@ -209,7 +220,12 @@ round(alpine_env_pca$CA$eig[1] / sum(alpine_env_pca$CA$eig) * 100, 1) # result i
 
 summary(alpine_env_pca)
 
+#Kaiser-Guttman criterion
+(ev <- alpine_env_pca$CA$eig) 
+ev[ev > mean(ev)] #only PC1 and PC2 useful in strongly displaying variation across sites  
+
 biplot(alpine_env_pca, scaling = 1, main = "PCA scaling 1", choices = c(1, 2))
+
 biplot(alpine_env_pca, scaling = 2, main = "PCA scaling 2", choices = c(1, 2))
 
 source("Quantitative_Ecology-main/Num_Ecol_R_book_ed1/cleanplot.pca.R")
@@ -224,8 +240,13 @@ ordisurf(alpine_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots
 
 #A
 #MOST IMPACTFUL VARIABLES 
-#PC1 and PC2 were only used as according to the summary of PCA its seen that the two principal components 
-#explain the bulk of variation across sites, cumulatively explaining 63% of variation while PC3 only explain about 19% of variation
+#PC1 and PC2 were used as according to the summary of PCA its seen that the two principal components 
+#cumulatively explaining 63% of variation across sites, this rather a low amount of variation accounted for in an ordination diagram 
+#therefore PC3, explain about 19% of remaining variation allowing for cumulative 83% of variation to be explained was considered upon observation. 
+
+#However to confirm the needed principal components for appropriate ordination display - the Kaiser - Guttman criterion was applied 
+#only PC1 and PC2 neded in ordination display 
+
 #in PC1 form has the highest positive while slope has the highest negative seen in summary-species scores 
 #the pca ordination display according to scaling 1 shows how slope and form strongly impacts the 
 #variation in environmental conditions across sites in accordance to its euclidean distance. 
@@ -237,7 +258,7 @@ ordisurf(alpine_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots
 #Due to variables, form and slope vectors being outside the radius of the ordination display 
 #they can strongly direct one towards patterns where form is high when slope is low 
 #or low slopes influence the high snow. 
-#hiigh form influences high snowmelt 
+#high form influences high snowmelt 
 
 #B
 #In accordance to the general shape of elevated regions the areas where the slop is lower, the form is higher as elevated landforms begin to take 
@@ -247,4 +268,6 @@ ordisurf(alpine_env_pca ~ T2C, birds_env_edit, add = TRUE, col = "salmon", knots
 #C.
 #In accordance to impactful variables identified above, there is a negative correlation by lower slope influencing higher form(common landform shapes in a elevated region)
 #this influences the positive correlation where high form has higher snowmelt as it shape will act as a catchment region for snow and its 
-#located at the lower region of the slope 
+#located at the lower region of the slope - seen by small angling, less that 90 degrees in scaling two among snow and form variable vectors 
+#aspect and slope is strongly positivly correlated seen in scaling two - this is as the slope inflences the magnitude of directionality of land 
+#towards the south simply through the influence of how the landform has shaped overtime through varius environmnetal processes 
